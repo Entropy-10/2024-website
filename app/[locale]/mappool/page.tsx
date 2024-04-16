26a27f216f1ea40551a59722bbd744642798997f
Entropy <45132530+Entropy-10@users.noreply.github.com>
2024-04-15 19:33:46 -0700

    chore: updated default mappool

--- app/[locale]/mappool/page.tsx
+++ app/[locale]/mappool/page.tsx
@@ -22,7 +22,7 @@ export default async function MappoolPage() {
 	const t = await getTranslations('MappoolPage')
 	const messages = await getMessages()
 	// TODO: fix this
-	const defaultRound = 'round of 32'
+	const defaultRound = 'round of 16'
 
 	return (
 		<div className='relative'>
