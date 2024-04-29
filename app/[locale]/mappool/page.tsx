1b87c695ceeef77429dc76551ba5b899675ee30d
Entropy <45132530+Entropy-10@users.noreply.github.com>
2024-04-29 08:58:42 -0700

    chore: updated default mappool round

--- app/[locale]/mappool/page.tsx
+++ app/[locale]/mappool/page.tsx
@@ -22,7 +22,7 @@ export default async function MappoolPage() {
 	const t = await getTranslations('MappoolPage')
 	const messages = await getMessages()
 	// TODO: fix this
-	const defaultRound = 'quarterfinals'
+	const defaultRound = 'semifinals'
 
 	return (
 		<div className='relative'>
